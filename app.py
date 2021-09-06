f13428aeeb7142c94586c6eae78e767b96505323
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2021-09-06 16:06:13 -0700

    Update files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
@@ -14,6 +14,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blogs.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "denisdziganchuk"
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Article(db.Model):
@@ -120,6 +129,12 @@ def post_delete(id):
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for("dashboard"))
     return render_template("login.html", form=form)
 
 
@@ -133,3 +148,9 @@ def register():
         db.session.commit()
         return redirect(url_for("login"))
     return render_template("registration.html", form=form)
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
